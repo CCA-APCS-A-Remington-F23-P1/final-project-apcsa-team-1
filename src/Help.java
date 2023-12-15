7a89f7307ef167678c04a833af05e3b7f63cda43
adev2142 <152904099+adev2142@users.noreply.github.com>
2023-12-15 17:44:00 +0000

    changes spacing in the help class

--- src/Help.java
+++ src/Help.java
@@ -4,7 +4,7 @@ import java.awt.event.ActionEvent;
 
 public class Help extends Scene {
   private static final String BUTTON_ACTION = "i was pressed";
-  
+
   public Help(){
     super();
     setOpaque(false);
@@ -22,17 +22,16 @@ public class Help extends Scene {
     super.paintComponent(frame);
 
     frame.drawString("Rules: ", 50, 50);
-    frame.drawString("You have to stop the prey before it reaches the treasure. In order to do this you need to select the appropriate predator in time.", 50, 60);
-    frame.drawString("List of predators and their corresponding prey:", 50, 60);
+    frame.drawString("You have to stop the prey before it reaches the treasure. In order to do this you need to select the appropriate predator in time.", 50, 80);
+    frame.drawString("List of predators and their corresponding prey:", 50, 110);
     frame.drawString("Eagle: Duck, Parrot, Toucan", 50, 70);
-    frame.drawString("Frog: Caterpillar, Ladybug, Grasshopper", 50, 80);
-    frame.drawString("Shark: Pufferfish, Clownfish, Octopus", 50, 90);
-    frame.drawString("Wolf: Pig, Sheep, Cow", 50, 100);
-    frame.drawString("Tiger: Deer, Reindeer, Moose", 50, 110);
-    frame.drawString("Snake: Mouse, Rat, Squirrel", 50, 120);
-    frame.drawString("Lion: Horse, Donkey, Zebra", 50, 130);
-    frame.drawString("T-Rex: Triceratops, Pterodactyl, Brachiosaurus", 50, 140);
-  }
+    frame.drawString("Frog: Caterpillar, Ladybug, Grasshopper", 50, 140);
+    frame.drawString("Shark: Pufferfish, Clownfish, Octopus", 50, 170);
+    frame.drawString("Wolf: Pig, Sheep, Cow", 50, 200);
+    frame.drawString("Tiger: Deer, Reindeer, Moose", 50, 230);
+    frame.drawString("Snake: Mouse, Rat, Squirrel", 50, 260);
+    frame.drawString("Lion: Horse, Donkey, Zebra", 50, 290);
+    frame.drawString("T-Rex: Triceratops, Pterodactyl, Brachiosaurus", 50, 320);   
 
   @Override
   public void actionPerformed(ActionEvent e) {
@@ -44,6 +43,6 @@ public class Help extends Scene {
       }
     }
     super.actionPerformed(e);
-    
+
   }
 }
