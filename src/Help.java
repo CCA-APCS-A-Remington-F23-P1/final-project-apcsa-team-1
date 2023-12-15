ac8ced8039b5552b97c8f92788d38d1ba8f6dddd
adev2142 <152904099+adev2142@users.noreply.github.com>
2023-12-15 17:15:34 +0000

    update spacing of help class

--- src/Help.java
+++ src/Help.java
@@ -24,14 +24,14 @@ public class Help extends Scene {
     frame.drawString("Rules: ", 50, 50);
     frame.drawString("You have to stop the prey before it reaches the treasure. In order to do this you need to select the appropriate predator in time.", 50, 80);
     frame.drawString("List of predators and their corresponding prey:", 50, 110);
-    frame.drawString("Eagle: Duck, Parrot, Toucan", 50, 70);
-    frame.drawString("Frog: Caterpillar, Ladybug, Grasshopper", 50, 140);
-    frame.drawString("Shark: Pufferfish, Clownfish, Octopus", 50, 170);
-    frame.drawString("Wolf: Pig, Sheep, Cow", 50, 200);
-    frame.drawString("Tiger: Deer, Reindeer, Moose", 50, 230);
-    frame.drawString("Snake: Mouse, Rat, Squirrel", 50, 260);
-    frame.drawString("Lion: Horse, Donkey, Zebra", 50, 290);
-    frame.drawString("T-Rex: Triceratops, Pterodactyl, Brachiosaurus", 50, 320);   
+    frame.drawString("Eagle: Duck, Parrot, Toucan", 50, 140);
+    frame.drawString("Frog: Caterpillar, Ladybug, Grasshopper", 50, 170);
+    frame.drawString("Shark: Pufferfish, Clownfish, Octopus", 50, 200);
+    frame.drawString("Wolf: Pig, Sheep, Cow", 50, 230);
+    frame.drawString("Tiger: Deer, Reindeer, Moose", 50, 260);
+    frame.drawString("Snake: Mouse, Rat, Squirrel", 50, 290);
+    frame.drawString("Lion: Horse, Donkey, Zebra", 50, 320);
+    frame.drawString("T-Rex: Triceratops, Pterodactyl, Brachiosaurus", 50, 350); 
 
   @Override
   public void actionPerformed(ActionEvent e) {
